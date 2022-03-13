5db5d6e34b566daa0b74604b9bb8e524bdf198e5
kvvsatyaravi <kvvsatyaravi318@gmail.com>
2022-03-13 06:43:02 +0530

    first

--- html.php
+++ html.php
@@ -1,3 +1,4 @@
+
 <html>
     <head>
         <!-- Latest compiled and minified CSS -->
@@ -6,7 +7,7 @@
     <body>
         <div class="container">
             <h4 style="text-align: center;" >Question paper</h4>
-            <div class="row">
+            <div class="row" style="margin-top: 25px;">
                 <label>Please Enter your name:</label> <input type="text" class="offset-1 col-3" placeholder="enter your name">
             </div>
             
@@ -14,18 +15,80 @@
                 <p>Choose the right answer</p>
                 <p>The smallest unit of matter that can exist independetely is</p>
                 <div class="item">
-                    <label>a)atom</label><input type="checkbox" class="" style="margin-left: 45px;" value="atom">
+                    <label class="col-2">a)atom</label><input type="checkbox" class=""  value="atom">
                 </div>
                 <div>
-                    <label>b)molecule</label><input type="checkbox" class="" style="margin-left: 17px;" value="molecule">
+                    <label class="col-2">b)molecule</label><input type="checkbox" class=""  value="molecule">
                 </div>
                 <div>
-                    <label>c)ion</label><input type="checkbox" style="margin-left: 60px;" value="ion">
+                    <label class="col-2">c)ion</label><input type="checkbox"  value="ion">
                 </div>
                 <div>
-                    <label>d)radical</label><input type="checkbox" style="
-                    margin-left: 33px;" value="radical">
+                    <label class="col-2">d)radical</label><input type="checkbox" value="radical">
                 </div>
+
+                <label><b>Question Model – 2 - Chose the Multiple correct answers</b></label>
+                <p>Choose all the options that apply.</p>
+                <p>Inside an atom there are</p>
+                <div>
+                    <label class="col-2">a)electrons</label><input type="checkbox" class=""  value="atom">
+                </div>
+                <div>
+                    <label class="col-2">b)protons</label><input type="checkbox" class=""  value="molecule">
+                </div>
+                <div>
+                    <label class="col-2">c)neutrons</label><input type="checkbox"  value="ion">
+                </div>
+                <div>
+                    <label class="col-2">d)molecules</label><input type="checkbox" value="radical">
+                </div>
+
+                <label><b>Question Model – 3 - Drop Down Option for correct answers</b></label>
+                <span>
+                    <p>The charge on an electron is 
+                        <select>
+                            <option value=””>positive</option>
+                            <option value=””>negative</option>
+                            <option value=””>neutral</option>
+                        </select> and the charge on a proton is 
+                        <select>
+                            <option value=””>positive</option>
+                            <option value=””>negative</option>
+                            <option value=””>neutral</option>
+                        </select>
+                    </p> 
+                </span>
+
+                <label><b>Question Model – 4 - Fill in the blanks with case sensitive correct
+                    answers</b></label>
+                    <span>
+                        <p>The SI unit of length, metre, is denoted by<input type="text" class="col-2">and the SI unit of electric
+                            current, ampere, is denoted by <input type="text" class="col-2"></p>
+                    </span>
+
+                <label><b>Question Model – 5 - Fill in the blanks with correct answers ( not Case
+                        sensitive</b></label>
+                    <p>A hundredth of a metre is a<input type="text" class="col-2"></p>
+
+                <label><b>Question Model –6 – Select True/ False of the given sentence .</b></label>
+                    <p>Mass and energy are interconvertible.</p>
+                    <p>Options:</p>
+                    <div class="item">
+                        <label class="col-2">TRUE</label><input type="checkbox" class=""  value="atom">
+                    </div>
+                    <div>
+                        <label class="col-2">FALSE</label><input type="checkbox" class=""  value="molecule">
+                    </div>
+                    <div>
+                        <label class="col-2">CANNOT BE DETERMINED</label><input type="checkbox"  value="ion">
+                    </div>
+
+                <label><b>Question Model – 7 : Match the left column (A) with that of Right
+                    Column ( B ).</b></label>
+                    <div><label class="col-2">length </label><input type="text" class="col-1"></div>
+                    <div><label class="col-2">mass </label><input type="text" class="col-1"></div>
+                    <div><label class="col-2">time </label><input type="text" class="col-1"></div>
+                    <div><label class="col-2">temperature </label><input type="text" class="col-1"></div>
         </div>
     </body>
 </html>
