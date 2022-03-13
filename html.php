66f31027d7afae945dbb1638aff3905b84682de1
kvvsatyaravi <kvvsatyaravi318@gmail.com>
2022-03-13 06:38:49 +0530

    front end over

---
+++ html.php
@@ -0,0 +1,31 @@
+<html>
+    <head>
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    </head>
+    <body>
+        <div class="container">
+            <h4 style="text-align: center;" >Question paper</h4>
+            <div class="row">
+                <label>Please Enter your name:</label> <input type="text" class="offset-1 col-3" placeholder="enter your name">
+            </div>
+            
+                <label><b>Question model -1 choose the single correct answer</b></label>
+                <p>Choose the right answer</p>
+                <p>The smallest unit of matter that can exist independetely is</p>
+                <div class="item">
+                    <label>a)atom</label><input type="checkbox" class="" style="margin-left: 45px;" value="atom">
+                </div>
+                <div>
+                    <label>b)molecule</label><input type="checkbox" class="" style="margin-left: 17px;" value="molecule">
+                </div>
+                <div>
+                    <label>c)ion</label><input type="checkbox" style="margin-left: 60px;" value="ion">
+                </div>
+                <div>
+                    <label>d)radical</label><input type="checkbox" style="
+                    margin-left: 33px;" value="radical">
+                </div>
+        </div>
+    </body>
+</html>
